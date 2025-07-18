a504a2244c27f93d0fe2668dd27ebd9d5d3dac10
shashank-99-code <59685878+shashank-99-code@users.noreply.github.com>
2025-07-19 05:00:53 +0530

    Fix book opening issue - only pass onClick when onBookClick is provided

--- src/components/book/BookGrid.tsx
+++ src/components/book/BookGrid.tsx
@@ -31,7 +31,11 @@ export function BookGrid({ books, onBookClick }: { books: Book[]; onBookClick?: 
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-6">
       {books.map((book) => (
-        <BookCard key={book.id} book={book} onClick={() => onBookClick?.(book)} />
+        <BookCard 
+          key={book.id} 
+          book={book} 
+          {...(onBookClick && { onClick: () => onBookClick(book) })}
+        />
       ))}
     </div>
   )
